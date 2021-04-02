1c2c54688a031a9a37a3c8f3f2a0ee168aacf7de
Hae-jin-park <phj30576@hotmail.com>
2021-04-02 17:21:18 +0900

    전략패턴의 예제 주석추가

--- EP00_StrategyPattern/src/study/example/Main.java
+++ EP00_StrategyPattern/src/study/example/Main.java
@@ -18,14 +18,18 @@ public class Main {
 //		2) Strategy 패턴 예제
 		GameCharacter character = new GameCharacter();
 		
+		//맨손공격 Weapon값이 null 즉 아무도 없기에.
 		character.attack();
 		
+		//칼로 공격
 		character.setWeapon(new Knife());
 		character.attack();
 
+		//검으로 공격
 		character.setWeapon(new Sword());
 		character.attack();
 		
+		//도끼로 공격
 		character.setWeapon(new Axe());
 		character.attack();
 	}
